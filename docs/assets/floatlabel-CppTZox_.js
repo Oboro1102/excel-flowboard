60d272a3f5a6270c043e2b02dc4ce062ab6bb413
Oboro <oboro1102@gmail.com>
2025-11-16 03:05:11 +0800

    打包專案

---
+++ docs/assets/floatlabel-CppTZox_.js
@@ -0,0 +1,183 @@
+import{M as e,Z as t,f as n,gt as r,j as i,p as a,ut as o,yt as s,z as c}from"./index-DRu-u0-8.js";var l={name:`BaseInput`,extends:{name:`BaseEditableHolder`,extends:n,emits:[`update:modelValue`,`value-change`],props:{modelValue:{type:null,default:void 0},defaultValue:{type:null,default:void 0},name:{type:String,default:void 0},invalid:{type:Boolean,default:void 0},disabled:{type:Boolean,default:!1},formControl:{type:Object,default:void 0}},inject:{$parentInstance:{default:void 0},$pcForm:{default:void 0},$pcFormField:{default:void 0}},data:function(){return{d_value:this.defaultValue===void 0?this.modelValue:this.defaultValue}},watch:{modelValue:{deep:!0,handler:function(e){this.d_value=e}},defaultValue:function(e){this.d_value=e},$formName:{immediate:!0,handler:function(e){var t,n;this.formField=((t=this.$pcForm)==null||(n=t.register)==null?void 0:n.call(t,e,this.$formControl))||{}}},$formControl:{immediate:!0,handler:function(e){var t,n;this.formField=((t=this.$pcForm)==null||(n=t.register)==null?void 0:n.call(t,this.$formName,e))||{}}},$formDefaultValue:{immediate:!0,handler:function(e){this.d_value!==e&&(this.d_value=e)}},$formValue:{immediate:!1,handler:function(e){var t;(t=this.$pcForm)!=null&&t.getFieldState(this.$formName)&&e!==this.d_value&&(this.d_value=e)}}},formField:{},methods:{writeValue:function(e,t){var n,r;this.controlled&&(this.d_value=e,this.$emit(`update:modelValue`,e)),this.$emit(`value-change`,e),(n=(r=this.formField).onChange)==null||n.call(r,{originalEvent:t,value:e})},findNonEmpty:function(){return[...arguments].find(c)}},computed:{$filled:function(){return c(this.d_value)},$invalid:function(){var e,t;return!this.$formNovalidate&&this.findNonEmpty(this.invalid,(e=this.$pcFormField)==null||(e=e.$field)==null?void 0:e.invalid,(t=this.$pcForm)==null||(t=t.getFieldState(this.$formName))==null?void 0:t.invalid)},$formName:function(){return this.$formNovalidate?void 0:this.name||this.$formControl?.name},$formControl:function(){return this.formControl||this.$pcFormField?.formControl},$formNovalidate:function(){return this.$formControl?.novalidate},$formDefaultValue:function(){var e;return this.findNonEmpty(this.d_value,this.$pcFormField?.initialValue,(e=this.$pcForm)==null||(e=e.initialValues)==null?void 0:e[this.$formName])},$formValue:function(){var e,t;return this.findNonEmpty((e=this.$pcFormField)==null||(e=e.$field)==null?void 0:e.value,(t=this.$pcForm)==null||(t=t.getFieldState(this.$formName))==null?void 0:t.value)},controlled:function(){return this.$inProps.hasOwnProperty(`modelValue`)||!this.$inProps.hasOwnProperty(`modelValue`)&&!this.$inProps.hasOwnProperty(`defaultValue`)},filled:function(){return this.$filled}}},props:{size:{type:String,default:null},fluid:{type:Boolean,default:null},variant:{type:String,default:null}},inject:{$parentInstance:{default:void 0},$pcFluid:{default:void 0}},computed:{$variant:function(){return this.variant??(this.$primevue.config.inputStyle||this.$primevue.config.inputVariant)},$fluid:function(){return this.fluid??!!this.$pcFluid},hasFluid:function(){return this.$fluid}}},u={name:`BaseInputText`,extends:l,style:a.extend({name:`inputtext`,style:`
+    .p-inputtext {
+        font-family: inherit;
+        font-feature-settings: inherit;
+        font-size: 1rem;
+        color: dt('inputtext.color');
+        background: dt('inputtext.background');
+        padding-block: dt('inputtext.padding.y');
+        padding-inline: dt('inputtext.padding.x');
+        border: 1px solid dt('inputtext.border.color');
+        transition:
+            background dt('inputtext.transition.duration'),
+            color dt('inputtext.transition.duration'),
+            border-color dt('inputtext.transition.duration'),
+            outline-color dt('inputtext.transition.duration'),
+            box-shadow dt('inputtext.transition.duration');
+        appearance: none;
+        border-radius: dt('inputtext.border.radius');
+        outline-color: transparent;
+        box-shadow: dt('inputtext.shadow');
+    }
+
+    .p-inputtext:enabled:hover {
+        border-color: dt('inputtext.hover.border.color');
+    }
+
+    .p-inputtext:enabled:focus {
+        border-color: dt('inputtext.focus.border.color');
+        box-shadow: dt('inputtext.focus.ring.shadow');
+        outline: dt('inputtext.focus.ring.width') dt('inputtext.focus.ring.style') dt('inputtext.focus.ring.color');
+        outline-offset: dt('inputtext.focus.ring.offset');
+    }
+
+    .p-inputtext.p-invalid {
+        border-color: dt('inputtext.invalid.border.color');
+    }
+
+    .p-inputtext.p-variant-filled {
+        background: dt('inputtext.filled.background');
+    }
+
+    .p-inputtext.p-variant-filled:enabled:hover {
+        background: dt('inputtext.filled.hover.background');
+    }
+
+    .p-inputtext.p-variant-filled:enabled:focus {
+        background: dt('inputtext.filled.focus.background');
+    }
+
+    .p-inputtext:disabled {
+        opacity: 1;
+        background: dt('inputtext.disabled.background');
+        color: dt('inputtext.disabled.color');
+    }
+
+    .p-inputtext::placeholder {
+        color: dt('inputtext.placeholder.color');
+    }
+
+    .p-inputtext.p-invalid::placeholder {
+        color: dt('inputtext.invalid.placeholder.color');
+    }
+
+    .p-inputtext-sm {
+        font-size: dt('inputtext.sm.font.size');
+        padding-block: dt('inputtext.sm.padding.y');
+        padding-inline: dt('inputtext.sm.padding.x');
+    }
+
+    .p-inputtext-lg {
+        font-size: dt('inputtext.lg.font.size');
+        padding-block: dt('inputtext.lg.padding.y');
+        padding-inline: dt('inputtext.lg.padding.x');
+    }
+
+    .p-inputtext-fluid {
+        width: 100%;
+    }
+`,classes:{root:function(e){var t=e.instance,n=e.props;return[`p-inputtext p-component`,{"p-filled":t.$filled,"p-inputtext-sm p-inputfield-sm":n.size===`small`,"p-inputtext-lg p-inputfield-lg":n.size===`large`,"p-invalid":t.$invalid,"p-variant-filled":t.$variant===`filled`,"p-inputtext-fluid":t.$fluid}]}}}),provide:function(){return{$pcInputText:this,$parentInstance:this}}};function d(e){"@babel/helpers - typeof";return d=typeof Symbol==`function`&&typeof Symbol.iterator==`symbol`?function(e){return typeof e}:function(e){return e&&typeof Symbol==`function`&&e.constructor===Symbol&&e!==Symbol.prototype?`symbol`:typeof e},d(e)}function f(e,t,n){return(t=p(t))in e?Object.defineProperty(e,t,{value:n,enumerable:!0,configurable:!0,writable:!0}):e[t]=n,e}function p(e){var t=m(e,`string`);return d(t)==`symbol`?t:t+``}function m(e,t){if(d(e)!=`object`||!e)return e;var n=e[Symbol.toPrimitive];if(n!==void 0){var r=n.call(e,t);if(d(r)!=`object`)return r;throw TypeError(`@@toPrimitive must return a primitive value.`)}return(t===`string`?String:Number)(e)}var h={name:`InputText`,extends:u,inheritAttrs:!1,methods:{onInput:function(e){this.writeValue(e.target.value,e)}},computed:{attrs:function(){return o(this.ptmi(`root`,{context:{filled:this.$filled,disabled:this.disabled}}),this.formField)},dataP:function(){return i(f({invalid:this.$invalid,fluid:this.$fluid,filled:this.$variant===`filled`},this.size,this.size))}}},g=[`value`,`name`,`disabled`,`aria-invalid`,`data-p`];function _(e,n,i,a,s,c){return r(),t(`input`,o({type:`text`,class:e.cx(`root`),value:e.d_value,name:e.name,disabled:e.disabled,"aria-invalid":e.$invalid||void 0,"data-p":c.dataP,onInput:n[0]||=function(){return c.onInput&&c.onInput.apply(c,arguments)}},c.attrs),null,16,g)}h.render=_;var v=e(),y=a.extend({name:`floatlabel`,style:`
+    .p-floatlabel {
+        display: block;
+        position: relative;
+    }
+
+    .p-floatlabel label {
+        position: absolute;
+        pointer-events: none;
+        top: 50%;
+        transform: translateY(-50%);
+        transition-property: all;
+        transition-timing-function: ease;
+        line-height: 1;
+        font-weight: dt('floatlabel.font.weight');
+        inset-inline-start: dt('floatlabel.position.x');
+        color: dt('floatlabel.color');
+        transition-duration: dt('floatlabel.transition.duration');
+    }
+
+    .p-floatlabel:has(.p-textarea) label {
+        top: dt('floatlabel.position.y');
+        transform: translateY(0);
+    }
+
+    .p-floatlabel:has(.p-inputicon:first-child) label {
+        inset-inline-start: calc((dt('form.field.padding.x') * 2) + dt('icon.size'));
+    }
+
+    .p-floatlabel:has(input:focus) label,
+    .p-floatlabel:has(input.p-filled) label,
+    .p-floatlabel:has(input:-webkit-autofill) label,
+    .p-floatlabel:has(textarea:focus) label,
+    .p-floatlabel:has(textarea.p-filled) label,
+    .p-floatlabel:has(.p-inputwrapper-focus) label,
+    .p-floatlabel:has(.p-inputwrapper-filled) label,
+    .p-floatlabel:has(input[placeholder]) label,
+    .p-floatlabel:has(textarea[placeholder]) label {
+        top: dt('floatlabel.over.active.top');
+        transform: translateY(0);
+        font-size: dt('floatlabel.active.font.size');
+        font-weight: dt('floatlabel.active.font.weight');
+    }
+
+    .p-floatlabel:has(input.p-filled) label,
+    .p-floatlabel:has(textarea.p-filled) label,
+    .p-floatlabel:has(.p-inputwrapper-filled) label {
+        color: dt('floatlabel.active.color');
+    }
+
+    .p-floatlabel:has(input:focus) label,
+    .p-floatlabel:has(input:-webkit-autofill) label,
+    .p-floatlabel:has(textarea:focus) label,
+    .p-floatlabel:has(.p-inputwrapper-focus) label {
+        color: dt('floatlabel.focus.color');
+    }
+
+    .p-floatlabel-in .p-inputtext,
+    .p-floatlabel-in .p-textarea,
+    .p-floatlabel-in .p-select-label,
+    .p-floatlabel-in .p-multiselect-label,
+    .p-floatlabel-in .p-multiselect-label:has(.p-chip),
+    .p-floatlabel-in .p-autocomplete-input-multiple,
+    .p-floatlabel-in .p-cascadeselect-label,
+    .p-floatlabel-in .p-treeselect-label {
+        padding-block-start: dt('floatlabel.in.input.padding.top');
+        padding-block-end: dt('floatlabel.in.input.padding.bottom');
+    }
+
+    .p-floatlabel-in:has(input:focus) label,
+    .p-floatlabel-in:has(input.p-filled) label,
+    .p-floatlabel-in:has(input:-webkit-autofill) label,
+    .p-floatlabel-in:has(textarea:focus) label,
+    .p-floatlabel-in:has(textarea.p-filled) label,
+    .p-floatlabel-in:has(.p-inputwrapper-focus) label,
+    .p-floatlabel-in:has(.p-inputwrapper-filled) label,
+    .p-floatlabel-in:has(input[placeholder]) label,
+    .p-floatlabel-in:has(textarea[placeholder]) label {
+        top: dt('floatlabel.in.active.top');
+    }
+
+    .p-floatlabel-on:has(input:focus) label,
+    .p-floatlabel-on:has(input.p-filled) label,
+    .p-floatlabel-on:has(input:-webkit-autofill) label,
+    .p-floatlabel-on:has(textarea:focus) label,
+    .p-floatlabel-on:has(textarea.p-filled) label,
+    .p-floatlabel-on:has(.p-inputwrapper-focus) label,
+    .p-floatlabel-on:has(.p-inputwrapper-filled) label,
+    .p-floatlabel-on:has(input[placeholder]) label,
+    .p-floatlabel-on:has(textarea[placeholder]) label {
+        top: 0;
+        transform: translateY(-50%);
+        border-radius: dt('floatlabel.on.border.radius');
+        background: dt('floatlabel.on.active.background');
+        padding: dt('floatlabel.on.active.padding');
+    }
+
+    .p-floatlabel:has([class^='p-'][class$='-fluid']) {
+        width: 100%;
+    }
+
+    .p-floatlabel:has(.p-invalid) label {
+        color: dt('floatlabel.invalid.color');
+    }
+`,classes:{root:function(e){var t=e.props;return[`p-floatlabel`,{"p-floatlabel-over":t.variant===`over`,"p-floatlabel-on":t.variant===`on`,"p-floatlabel-in":t.variant===`in`}]}}}),b={name:`FloatLabel`,extends:{name:`BaseFloatLabel`,extends:n,props:{variant:{type:String,default:`over`}},style:y,provide:function(){return{$pcFloatLabel:this,$parentInstance:this}}},inheritAttrs:!1};function x(e,n,i,a,c,l){return r(),t(`span`,o({class:e.cx(`root`)},e.ptmi(`root`)),[s(e.$slots,`default`)],16)}b.render=x;export{l as i,v as n,h as r,b as t};
---
+++ docs/assets/useDnD-BMAUVTNM.js
@@ -0,0 +1,11 @@
+import{Et as e,Lt as t}from"./index-DRu-u0-8.js";import{u as n}from"./vue-flow-core-C04USpd_.js";var r=()=>`dndnode_${crypto.randomUUID()}`,i={draggedType:t(null),isDragOver:t(!1),isDragging:t(!1)},a=t(null);function o(){let{draggedType:t,isDragOver:o,isDragging:s}=i,{addNodes:c,screenToFlowCoordinate:l,onNodesInitialized:u,updateNode:d}=n();e(s,e=>{document.body.style.userSelect=e?`none`:``});function f(e,n){if(e.dataTransfer&&(e.dataTransfer.setData(`application/vueflow`,n),e.dataTransfer.effectAllowed=`move`,e.target instanceof HTMLElement)){let t=e.target,n=t.cloneNode(!0);a.value=`vueflow-drag-image-${Date.now()}-${Math.random().toString(36).substring(2,9)}`,n.id=a.value,n.style.cssText=`
+          width: ${t.offsetWidth}px;
+          height: ${t.offsetHeight}px;
+          opacity: 0.7;
+          background-color: white;
+          pointer-events: none;
+          position: absolute;
+          z-index: 9999;
+          left: -1000px;
+          top: -1000px;
+        `,document.body.appendChild(n),e.dataTransfer.setDragImage(n,t.offsetWidth/2,t.offsetHeight/2)}t.value=n,s.value=!0,document.addEventListener(`drop`,h)}function p(e){e.preventDefault(),t.value&&(o.value=!0,e.dataTransfer&&(e.dataTransfer.dropEffect=`move`))}function m(){o.value=!1}function h(){if(s.value=!1,o.value=!1,t.value=null,document.removeEventListener(`drop`,h),a.value){let e=document.getElementById(a.value);e&&e.parentNode===document.body&&document.body.removeChild(e),a.value=null}}function g(e){let n=l({x:e.clientX,y:e.clientY}),i=r(),a={id:i,type:t.value,position:n,data:{label:i,type:t.value}},{off:o}=u(()=>{d(i,e=>({position:{x:e.position.x-e.dimensions.width/2,y:e.position.y-e.dimensions.height/2}})),o()});c(a)}return{draggedType:t,isDragOver:o,isDragging:s,onDragStart:f,onDragLeave:m,onDragOver:p,onDrop:g}}export{o as t};
