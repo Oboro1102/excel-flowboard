f15702d953b919d243ecd4a8154b8f12b0730bee
Oboro <oboro1102@gmail.com>
2026-01-08 22:03:56 +0800

    調整登入畫面視覺

--- src/layout/LayoutLogin.vue
+++ src/layout/LayoutLogin.vue
@@ -10,7 +10,9 @@ const { showLogo } = toRefs(props)
 </script>
 
 <template>
-  <main class="flex grow flex-col items-center justify-center">
+  <main
+    class="flex grow flex-col items-center justify-center bg-[url(https://unsplash.com/photos/seA-FPPXL-M/download?force=true&w=1920)] bg-cover bg-center p-4"
+  >
     <div class="radius wrap--gap flex w-full max-w-md flex-col bg-white p-6 shadow-lg">
       <div v-if="showLogo" class="text-main mx-auto flex items-center gap-2">
         <span class="text-2xl font-semibold">Excel 圖表白板</span>
